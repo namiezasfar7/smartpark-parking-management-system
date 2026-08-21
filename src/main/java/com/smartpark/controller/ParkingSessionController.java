2b23433bb18872fce61ed63ff58498a9e6dce7eb
Namiez Asfar <namiez.asfar79@gmail.com>
2026-08-21 18:14:20 +0530

    Create ParkingSessionController domain model

---
+++ src/main/java/com/smartpark/controller/ParkingSessionController.java
@@ -0,0 +1,40 @@
+package com.smartpark.controller;
+
+//IMPORTS
+import com.smartpark.model.ParkingSession;
+import com.smartpark.service.ParkingSessionService;
+
+import java.util.List;
+
+//PARKING SESSION CONTROLLER CLASS
+public class ParkingSessionController {
+
+    //DECLARE ATTRIBUTES
+    private ParkingSessionService parkingSessionService;
+
+    //DECLARE CONSTRUCTOR
+    public ParkingSessionController(ParkingSessionService parkingSessionService) {
+        this.parkingSessionService = parkingSessionService;
+    }
+
+    //DECLARE METHODS
+    //START PARKING SESSION
+    public void startSession(ParkingSession session) {
+        parkingSessionService.startSession(session);
+    }
+
+    //FIND PARKING SESSION
+    public ParkingSession findSession(String sessionId) {
+        return parkingSessionService.findSession(sessionId);
+    }
+
+    //GET ALL PARKING SESSIONS
+    public List <ParkingSession> getAllSessions() {
+        return parkingSessionService.getAllSessions();
+    }
+
+    //COMPLETE SESSION
+    public void completeSession(String sessionId) {
+        parkingSessionService.completeSession(sessionId);
+    }
+}
