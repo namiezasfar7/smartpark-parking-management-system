2b7e7a43bf3ef65d10d756668ba89aa545ee1281
Lakmina Perera <lakminaudayangana2021@gmail.com>
2026-08-26 15:50:01 +0530

    Create parking zone model

--- src/main/java/com/smartpark/Main.java
+++ src/main/java/com/smartpark/Main.java
@@ -7,14 +7,17 @@ import com.smartpark.controller.ParkingSessionController;
 import com.smartpark.controller.VehicleController;
 
 import com.smartpark.model.ParkingSpace;
+import com.smartpark.model.ParkingZone;
 import com.smartpark.model.VehicleType;
 
 import com.smartpark.repository.ParkingSessionRepository;
 import com.smartpark.repository.ParkingSpaceRepository;
+import com.smartpark.repository.ParkingZoneRepository;
 import com.smartpark.repository.VehicleRepository;
 
 import com.smartpark.repository.memory.InMemoryParkingSessionRepository;
 import com.smartpark.repository.memory.InMemoryParkingSpaceRepository;
+import com.smartpark.repository.memory.InMemoryParkingZoneRepository;
 import com.smartpark.repository.memory.InMemoryVehicleRepository;
 
 import com.smartpark.service.AnalyticsService;
@@ -36,10 +39,11 @@ public class Main {
             //CREATE REPOSITORIES
             VehicleRepository vehicleRepository = new InMemoryVehicleRepository();
             ParkingSpaceRepository parkingSpaceRepository = new InMemoryParkingSpaceRepository();
+            ParkingZoneRepository parkingZoneRepository = new InMemoryParkingZoneRepository();
             ParkingSessionRepository parkingSessionRepository = new InMemoryParkingSessionRepository();
 
-            //CREATE INITIAL PARKING SPACES
-            createInitialParkingSpaces(parkingSpaceRepository);
+            //CREATE INITIAL PARKING SPACES AND ZONES
+            createInitialParkingSpaces(parkingSpaceRepository, parkingZoneRepository);
 
             //CREATE SERVICES
             VehicleService vehicleService = new VehicleService(vehicleRepository);
@@ -51,7 +55,7 @@ public class Main {
 
             //CREATE CONTROLLERS
             VehicleController vehicleController = new VehicleController(vehicleService);
-            ParkingController parkingController = new ParkingController(parkingService);
+            ParkingController parkingController = new ParkingController(parkingService, parkingZoneRepository);
             ParkingSessionController parkingSessionController = new ParkingSessionController(parkingSessionService);
             DashboardController dashboardController = new DashboardController(analyticsService);
 
@@ -69,16 +73,43 @@ public class Main {
         });
     }
 
-    //CREATE INITIAL PARKING SPACES
-    private static void createInitialParkingSpaces(ParkingSpaceRepository repository) {
+    //CREATE INITIAL PARKING SPACES AND ZONES
+    private static void createInitialParkingSpaces(ParkingSpaceRepository spaceRepository,
+                                                   ParkingZoneRepository zoneRepository) {
 
-        repository.save(new ParkingSpace("P-001", "GF", VehicleType.CAR));
-        repository.save(new ParkingSpace("P-002", "GF", VehicleType.CAR));
-        repository.save(new ParkingSpace("P-003", "GF", VehicleType.CAR));
-        repository.save(new ParkingSpace("P-004", "GF", VehicleType.CAR));
-        repository.save(new ParkingSpace("P-005", "GF", VehicleType.CAR));
-        repository.save(new ParkingSpace("P-006", "GF", VehicleType.CAR));
-        repository.save(new ParkingSpace("P-007", "GF", VehicleType.CAR));
-        repository.save(new ParkingSpace("P-008", "GF", VehicleType.CAR));
+        //CREATE ZONES
+        ParkingZone groundFloor = new ParkingZone("GROUND", "Ground Floor");
+        ParkingZone level1 = new ParkingZone("LEVEL1", "Level 1");
+        ParkingZone level2 = new ParkingZone("LEVEL2", "Level 2");
+        ParkingZone level3 = new ParkingZone("LEVEL3", "Level 3");
+
+        //FILL EACH ZONE WITH 8 SPACES, NUMBERED SEQUENTIALLY ACROSS ALL ZONES
+        int nextSpaceNumber = 1;
+        nextSpaceNumber = addSpacesToZone(groundFloor, nextSpaceNumber, 8, spaceRepository);
+        nextSpaceNumber = addSpacesToZone(level1, nextSpaceNumber, 8, spaceRepository);
+        nextSpaceNumber = addSpacesToZone(level2, nextSpaceNumber, 8, spaceRepository);
+        addSpacesToZone(level3, nextSpaceNumber, 8, spaceRepository);
+
+        //SAVE ZONES
+        zoneRepository.save(groundFloor);
+        zoneRepository.save(level1);
+        zoneRepository.save(level2);
+        zoneRepository.save(level3);
+    }
+
+    //ADD SPACES TO ZONE
+    private static int addSpacesToZone(ParkingZone zone, int startNumber, int count,
+                                       ParkingSpaceRepository spaceRepository) {
+
+        //LOOP UNTIL CONDITION IS TRUE
+        for (int i = 0; i < count; i++) {
+
+            ParkingSpace space = new ParkingSpace(String.format("P-%03d", startNumber + i), VehicleType.CAR);
+
+            zone.addSpace(space);
+            spaceRepository.save(space);
+        }
+
+        return startNumber + count;
     }
 }
